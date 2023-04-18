9a9c6f5b371cbcf099089f9554d7aebab9977e78
Bhagyashree <khairnarb101@gmail.com>
2023-04-18 14:52:40 +0530

    Expense app initial project set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './components/ExpenseItem';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem> 
+    </div>
+  );
+}
+
+export default App;
